8c3f390d394267e20a37231dcff7e56687241bc3
Wouter-vdW <wouter.vanderwielen@gmail.com>
2019-07-07 13:51:20 -0500

    Computation - Problem sets Week 1

---
+++ ProblemSets/Computation/PS1/PS1b/calculator.py
@@ -0,0 +1,12 @@
+# calculator.py
+
+def summing(in1,in2):
+    result = in1 + in2
+    return result
+
+def product(in1,in2):
+    result2 = in1 * in2
+    return result2
+
+if __name__ == "__main__":
+    print()
